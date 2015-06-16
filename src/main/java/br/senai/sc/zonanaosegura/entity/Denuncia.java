87c16e4431a38b65b3069eb3a58b5352a79f3a44
gabriel_segalla <gabriel_segalla@SNFNS2EP14VM044.florianopolis.sc.senai.br>
2015-06-15 22:30:27 -0300

    Configuraçoes gerais

--- src/main/java/br/senai/sc/zonanaosegura/entity/Denuncia.java
+++ src/main/java/br/senai/sc/zonanaosegura/entity/Denuncia.java
@@ -11,11 +11,12 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Denuncia {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
 	private Date date;
+	private String horario;
 	private String descricao;
 	@ManyToOne
 	private Local local;
@@ -23,42 +24,61 @@ public class Denuncia {
 	private List<Objeto> objetos;
 	@ManyToOne
 	private TipoCrime tipo;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public Local getLocal() {
 		return local;
 	}
+
 	public void setLocal(Local local) {
 		this.local = local;
 	}
+
 	public List<Objeto> getObjetos() {
 		return objetos;
 	}
+
 	public void setObjetos(List<Objeto> objetos) {
 		this.objetos = objetos;
 	}
+
 	public TipoCrime getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(TipoCrime tipo) {
 		this.tipo = tipo;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
 	}
 
 }
